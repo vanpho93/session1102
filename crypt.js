defb9851383b57daff5bd3621413722e923e7527
Admin <vanpho02@gmail.com>
2017-03-18 16:17:39 +0700

    OK

---
+++ crypt.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcrypt');
+
+// bcrypt.hash('khoapham', 10, (err, encrypted) => {
+//     console.log(encrypted);
+// });
+
+const encrypted = '$2a$10$FfzkiXHdRtJTnmuAR0yyA.B5D6H4k5Nkx8wUGgo6EdGfwBZTTBKGa';
+
+bcrypt.compare('khoapham123', encrypted, (err, same) => {
+    console.log(err);
+    console.log(same);
+});
